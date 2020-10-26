a2a1cd0a59a3cce013e68cfa83b2f082aa069db3
Henrique Barros de Almeida <henrique.almeida4@fatec.sp.gov.br>
2020-10-26 14:44:56 -0300

    Project Start

---
+++ BowlingCounter.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BowlingCounter.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
