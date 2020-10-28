b4756989fd8d599f7acc67980ce14db4af284620
Henrique Barros de Almeida <henrique.almeida4@fatec.sp.gov.br>
2020-10-28 13:40:24 -0300

    test: Added Spare scenery to point calculation test

--- BowlingCounterApp.Tests/BowlingCounterTest.cs
+++ BowlingCounterApp.Tests/BowlingCounterTest.cs
@@ -15,10 +15,24 @@ namespace BowlingCounterApp.Tests
             bowlingCounter.PlayFrame((int i) => 10);
             bowlingCounter.PlayFrame((int i) => 10);
             bowlingCounter.PlayFrame((int i) => 10);
+            bowlingCounter.PlayFrame((int i) => 5);
+            bowlingCounter.PlayFrame((int i) => 5);
+            bowlingCounter.PlayFrame((int i) => 4);
+            bowlingCounter.PlayFrame((int i) => 3);
+            bowlingCounter.PlayFrame((int i) => 2);
+            bowlingCounter.PlayFrame((int i) => 1);
+            bowlingCounter.PlayFrame((int i) => 0);
 
             Assert.Equal(30, bowlingCounter.frames[0].Points);
-            Assert.Equal(20, bowlingCounter.frames[1].Points);
-            Assert.Equal(10, bowlingCounter.frames[2].Points);
+            Assert.Equal(30, bowlingCounter.frames[1].Points);
+            Assert.Equal(30, bowlingCounter.frames[2].Points);
+            Assert.Equal(20, bowlingCounter.frames[3].Points);
+            Assert.Equal(14, bowlingCounter.frames[4].Points);
+            Assert.Equal(8, bowlingCounter.frames[5].Points);
+            Assert.Equal(6, bowlingCounter.frames[6].Points);
+            Assert.Equal(4, bowlingCounter.frames[7].Points);
+            Assert.Equal(2, bowlingCounter.frames[8].Points);
+            Assert.Equal(0, bowlingCounter.frames[9].Points);
         }
     }
 }
