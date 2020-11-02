78f83513f1b53c0f87f007c0fc87ea55e177bceb
Henrique Barros de Almeida <henrique.almeida4@fatec.sp.gov.br>
2020-11-02 20:49:24 -0300

    refactor: improved scoring logic

--- BowlingCounterApp.Core/BowlingCounter.cs
+++ BowlingCounterApp.Core/BowlingCounter.cs
@@ -6,15 +6,26 @@ namespace BowlingCounterApp.Core
 {
     public class BowlingCounter
     {
-        private int _score = 0;
+
+        private List<int> rollsPerformed { get; set; } = new List<int>(21);
         public void throwBall(int pinsDropped)
         {
-            _score += pinsDropped;
+            rollsPerformed.Add(pinsDropped);
         }
 
         public int TotalScore()
         {
-            return _score;
+            var score = 0;
+
+            var ballIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                score += this.rollsPerformed[ballIndex] + this.rollsPerformed[ballIndex + 1];
+                ballIndex += 2;
+            }
+
+            return score;
         }
     };
 }
