525d71033b931207533d035bf3607cd197b37669
Henrique Barros de Almeida <henrique.almeida4@fatec.sp.gov.br>
2020-11-02 22:31:10 -0300

    refactor: imrpoved logic with score helper methods

--- BowlingCounterApp.Core/BowlingCounter.cs
+++ BowlingCounterApp.Core/BowlingCounter.cs
@@ -22,22 +22,37 @@ namespace BowlingCounterApp.Core
 
             for (int frame = 0; frame < 10; frame++)
             {
-                if (rollsPerformed[rollIndex] == 10)
+                if (rollsPerformed[rollIndex] == 10) // * Strike
                 {
-                    score += 10 + rollsPerformed[rollIndex + 1] + rollsPerformed[rollIndex + 2];
+                    score += 10 + calculateStrikeBonus(rollIndex);
                     rollIndex--;
                 }
                 else if (isSpare(rollIndex))
                 {
-                    score += 10 + rollsPerformed[rollIndex + 2];
+                    score += 10 + calculateSpareBonus(rollIndex);
                 }
                 else
-                    score += rollsPerformed[rollIndex] + rollsPerformed[rollIndex + 1];
+                    score += sumOfPinsDroppedInFrame(rollIndex);
 
                 rollIndex += 2;
             }
 
             return score;
+        }
+
+        private int sumOfPinsDroppedInFrame(int rollIndex)
+        {
+            return rollsPerformed[rollIndex] + rollsPerformed[rollIndex + 1];
+        }
+
+        private int calculateSpareBonus(int rollIndex)
+        {
+            return rollsPerformed[rollIndex + 2];
+        }
+
+        private int calculateStrikeBonus(int rollIndex)
+        {
+            return rollsPerformed[rollIndex + 1] + rollsPerformed[rollIndex + 2];
         }
 
         private bool isSpare(int rollIndex)
